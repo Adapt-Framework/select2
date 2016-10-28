e242491fd0e615c792dcceaed07074c192761767
sion purnell <sp@deliowealth.com>
2016-10-28 16:24:17 +0100

    fix file paths

--- boot.php
+++ boot.php
@@ -8,7 +8,7 @@ defined('ADAPT_STARTED') or die;
 
 $adapt = $GLOBALS['adapt'];
 
-$adapt->dom->head->add(new adapt\html_link(array('type' => 'text/css', 'rel' => 'stylesheet', 'href' => '/adapt/extensions/select2/static/css/select2.min.css.css')));
+$adapt->dom->head->add(new adapt\html_link(array('type' => 'text/css', 'rel' => 'stylesheet', 'href' => '/adapt/extensions/select2/static/css/select2.min.css')));
 $adapt->dom->head->add(new html_script(array('type' => 'text/javascript', 'src' => '/adapt/extensions/select2/static/js/select2.min.js')));
 $adapt->dom->head->add(
     new html_script("\$(document).ready(function(){ \$('.view.select2').select2(); });", array('type' => 'text/javascript'))
--- classes/bundle_select2.php
+++ classes/bundle_select2.php
@@ -14,7 +14,7 @@ namespace select2{
         public function boot(){
             if (parent::boot()){
                 
-                $this->dom->head->add(new adapt\html_link(array('type' => 'text/css', 'rel' => 'stylesheet', 'href' => "/adapt/select2/select2-{$this->version}/static/css/select2.min.css.css")));
+                $this->dom->head->add(new adapt\html_link(array('type' => 'text/css', 'rel' => 'stylesheet', 'href' => "/adapt/select2/select2-{$this->version}/static/css/select2.min.css")));
                 $this->dom->head->add(new html_script(array('type' => 'text/javascript', 'src' => "/adapt/select2/select2-{$this->version}/static/js/select2.min.js")));
                 $this->dom->head->add(
                     new html_script("\$(document).ready(function(){ \$('.view.select2').select2(); });", array('type' => 'text/javascript'))
