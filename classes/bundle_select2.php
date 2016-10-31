f3cabd5de344b2ff6ad0ccb1bd1a48cd408087d0
sion purnell <sp@deliowealth.com>
2016-10-31 16:07:06 +0000

    fix names

--- classes/bundle_select2.php
+++ classes/bundle_select2.php
@@ -16,9 +16,9 @@ namespace select2{
                 
                 $this->dom->head->add(new adapt\html_link(array('type' => 'text/css', 'rel' => 'stylesheet', 'href' => "/adapt/select2/select2-{$this->version}/static/css/select2.min.css")));
                 $this->dom->head->add(new html_script(array('type' => 'text/javascript', 'src' => "/adapt/select2/select2-{$this->version}/static/js/select2.min.js")));
-                $this->dom->head->add(
-                    new html_script("\$(document).ready(function(){ \$('.view.select2').select2(); });", array('type' => 'text/javascript'))
-                );
+//                $this->dom->head->add(
+//                    new html_script("\$(document).ready(function(){ \$('.view.select2').select2(); });", array('type' => 'text/javascript'))
+//                );
                 
                 return true;
             }
